9157ccacfade228ab69b33367d5fe68b082e20df
Emanuel Christo <emanuelchristochris@gmail.com>
2021-01-23 11:35:24 +0530

    Initialized svelte app, did the basic blog feed

---
+++ src/App.svelte
@@ -0,0 +1,73 @@
+<script>
+    import PostCard from './PostCard.svelte'
+</script>
+
+<header>
+    <div class="container">
+        <h1>The Blog.</h1>
+        <div class="options-container">
+            <div class="sortby-container">
+                <p>Sort by</p>
+                <select>
+                    <option value="latest">latest</option>
+                    <option value="most read">most read</option>
+                    <option value="oldest">oldest</option>
+                </select>
+            </div>
+            <div class="serach-container">
+                <input type="text" placeholder="search" />
+            </div>
+        </div>
+    </div>
+</header>
+
+<main>
+    <div class="container">
+        <PostCard/>
+        <PostCard/>
+        <PostCard/>
+        <PostCard/>
+    </div>
+</main>
+
+<footer></footer>
+
+<style>
+    h1 {
+        margin-top: 50px;
+        font-family: Rockwell;
+        font-size: 1.4em;
+    }
+
+    .container {
+        width: 80%;
+        margin: 0 auto;
+    }
+
+    .options-container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 20px;
+        border-bottom: 1px solid #000;
+    }
+
+    .sortby-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    select {
+        border: none;
+        padding: 10px;
+        border-bottom: 1px solid #000;
+        margin-left: 15px;
+    }
+
+    input {
+        border: none;
+        padding: 10px;
+        border-bottom: 1px solid #000;
+    }
+</style>
---
+++ src/PostCard.svelte
@@ -0,0 +1,58 @@
+<script>
+
+</script>
+
+<div class="contianer">
+    <div class="left">
+        <p>3 Jan 2021</p>
+        <h2>Is it time to regulate big tech?</h2>
+    </div>
+    <div class="right">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut auctor dolor. Nullam ornare leo massa, et tempus felis viverra a. Duis aliquet suscipit elit vestibulum accumsan. Morbi condimentum consequat augue, a vehicula nunc mattis non. Quisque nec leo id neque facilisis convallis. Nam maximus sed sem id tincidunt. Aenean libero sem, tempus sed risus eget, tristique tincidunt mi. Nulla aliquet nibh et sollicitudin blandit.</p>
+        <div class="more-wrapper"><a href="body">more</a></div>
+    </div>
+</div>
+
+<style>
+    .contianer {
+        display: flex;
+        padding: 40px 25px 100px 25px;
+        border-bottom: 1px solid #000;
+    }
+    
+    .left {
+        width: 30%;
+    }
+
+    .right {
+        width: 70%;
+        margin-left: 100px;
+    }
+
+    .left p {
+        all: unset;
+        color: #ff0000;
+        font-size: 10pt;
+    }
+
+    h2 {
+        font-family: Rockwell;
+        margin: 0;
+        margin-top: 15px;
+        font-size: 2.2em;
+    }
+
+    .right p {
+        margin: 0;
+        margin-top: 30px;
+        letter-spacing: 0.5px;
+    }
+
+    a {
+        color: #0062ff;
+    }
+
+    .more-wrapper {
+        margin-top: 10px;
+    }
+</style>
